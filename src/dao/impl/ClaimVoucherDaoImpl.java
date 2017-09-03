bc91911991220e1be9a50f4bf53f29b3d5e64de1
bowieu <fatalitywolf@foxmail.com>
2017-09-03 10:38:46 +0800

    相应的方法的使用和hibernate配置文件与spring的整合

--- src/dao/impl/ClaimVoucherDaoImpl.java
+++ src/dao/impl/ClaimVoucherDaoImpl.java
@@ -1,5 +1,7 @@
 package dao.impl;
 
+import java.util.Date;
+
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import dao.ClaimVoucherDao;
@@ -11,6 +13,28 @@ public class ClaimVoucherDaoImpl extends HibernateDaoSupport implements ClaimVou
 	public void save(BizClaimVoucher claimVoucher) {
 		
 		this.getHibernateTemplate().save(claimVoucher);
+		
+	}
+	//save方法
+	
+	public void show(int i ,String...values) {
+		
+		this.getHibernateTemplate().bulkUpdate("queryString ? ? ?",1,"a","b");
+		this.getHibernateTemplate().clear();
+		//清除对象，使对象处于脱机状态 
+		this.getHibernateTemplate().delete(null);
+		//删除对象
+		this.getHibernateTemplate().deleteAll(null);
+		//删除所有对象
+		this.getHibernateTemplate().evict(null);
+		//evacuate也是清除对象
+		this.getHibernateTemplate().find("from ? ? ?",1,"bac",new Date());
+		//查找方法
+		this.getHibernateTemplate().findByCriteria(null,1,1);
+		//通过Criteria查询
+		this.getHibernateTemplate().findByExample(null);
+		//通过简单对象查询，提取对象中的包含值的属性，但是不考虑主键和外键，而且条件运算符有限制。只能为==或者为like大于或者小于不适用
+		
 	}
 
 }
